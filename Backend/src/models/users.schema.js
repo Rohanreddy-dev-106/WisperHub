4ca79f3150074c5556c403ce36c0226b1e7ff575
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2026-02-13 16:49:17 +0530

    User_schema_creation_completed

---
+++ Backend/src/models/users.schema.js
@@ -0,0 +1,69 @@
+import mongoose, { Schema } from "mongoose";
+
+const UsersSchema = new Schema({
+    UserName: {
+        type: String,
+        minlength: 5,
+        maxlength: 20,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    Uniqueid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    Ip: {
+        type: String,
+        default: null
+    },
+
+    IpHistory: [{
+        type: String
+    }],
+
+    UserDevice: {
+        type: String,
+        default: null
+    },
+
+    isBanned: {
+        type: Boolean,
+        default: false
+    },
+
+    Avatar: {
+        type: String,
+        default: null
+    },
+
+    Banreason: {
+        type: String,
+        minlength: 10,
+        maxlength: 100,
+        default: null
+    },
+
+    Role: {
+        type: String,
+        required: true,
+        enum: ["user", "admin", "manager"],
+        default: "user"
+    },
+
+    RefreshToken: {
+        type: String,
+        default: null
+    },
+
+    AccessToken: {
+        type: String,
+        default: null
+    }
+
+}, { timestamps: true });
+
+export default mongoose.model("User", UsersSchema);
