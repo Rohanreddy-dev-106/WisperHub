175abde4c4fb57b65afcc74e80d60f9e5fbdc0b7
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2026-02-13 16:25:16 +0530

    example

---
+++ Backend/example.db.js
@@ -0,0 +1,29 @@
+/**
+ * Example file
+ * ------------
+ * This file contains default username IDs used for generating anonymous users.
+ * 
+ * NOTE:
+ * - This is an example/reference file.
+ * - To insert or modify names in production,
+ *   allow access only to admin or manager-level users.
+ */
+
+const userIds = {
+  firstNames: [
+    "Aren",
+    "Levi",
+    "Mikasa",
+    "Naruto",
+    "Gojo"
+  ],
+  lastNames: [
+    "Yeager",
+    "Ackerman",
+    "Uchiha",
+    "Uzumaki",
+    "Satoru"
+  ]
+};
+
+export default userIds;
