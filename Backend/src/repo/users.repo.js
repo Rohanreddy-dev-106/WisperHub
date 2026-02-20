e9aab4e3b7b1f318910a1f1b14c7321e00ba074e
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2026-02-20 17:53:29 +0530

    users-layer

--- Backend/src/repo/users.repo.js
+++ Backend/src/repo/users.repo.js
@@ -2,9 +2,17 @@ import userModel from "../models/users.schema.js"
 
 export default class Userrepository{
     async register(data){
-
+      try {
+        
+      } catch (error) {
+        
+      }
     }
     async finduser(data){
-
+      try {
+        
+      } catch (error) {
+        
+      }
     }
 }
