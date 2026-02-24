1c44ada7745b00bd1a00aa5757093524e213779b
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2026-02-24 23:58:52 +0530

    multer-config-don

---
+++ Backend/src/middlewares/multer.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import path from "path";
+const strorageConfing = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/temp/");
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        const ext = path.extname(file.originalname);
+        cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+    }
+})
+const Userfile = multer({
+    storage: strorageConfing
+})
+
+export default Userfile;
--- Backend/src/routes/post.routs.js
+++ Backend/src/routes/post.routs.js
@@ -1,16 +1,16 @@
 import express from "express";
 import PostController from "../controllers/post.controller.js";
 import jwtAuth from "../middlewares/jwtAuth.js";
-
+import multerUplode from "../middlewares/multer.js"
 const PostRoutes = express.Router();
 
 const Post = new PostController();
 
-PostRoutes.post("/create", jwtAuth, (req, res, next) => {
+PostRoutes.post("/create", jwtAuth, multerUplode.single("media"), (req, res, next) => {
     Post.createPost(req, res, next);
 });
 
-PostRoutes.put("/update/:postId", jwtAuth, (req, res, next) => {
+PostRoutes.put("/update/:postId", jwtAuth, multerUplode.single("media"), (req, res, next) => {
     Post.updatePost(req, res, next);
 });
 
